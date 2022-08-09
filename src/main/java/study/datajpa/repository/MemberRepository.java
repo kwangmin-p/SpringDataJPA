1af5ce6c8415293f6a6714e83dc01d4aa3ba928d
gmpark <huiae07@gmail.com>
2022-08-09 22:22:24 +0900

    Query annotation

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -15,4 +15,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query(name = "Member.findByUsername")  // 사실 여기에선 spring data jpa에서는 @Query 를 주석쳐도 된다. findByUsername이라는 namedQuery를 먼저 찾는것이 관계이기 때문이다. NamedQuery가 없다면 메소드 이름으로 쿼리를 생성한다. 즉 우선순위 1.NamedQuery 2.Method명으로 쿼리 생성
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
